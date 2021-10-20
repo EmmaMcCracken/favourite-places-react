e63977a3ecbb9daf92c90d621254b159ab5d0206
Emma <emma.mccracken14@gmail.com>
2021-10-20 16:25:34 +0100

    changed image

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -13,7 +13,7 @@ function MainContent(): JSX.Element {
         title="Dear Green Place"
         name={"Glasgow"}
         country="Scotland"
-        image="https://www.arup.com/-/media/arup/images/projects/u/university-of-glasgow-net-zero-carbon-strategy/university-of-glasgow-main-building-288-2.jpg?"
+        image="https://images.unsplash.com/photo-1621519604512-85ea63c15ca2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1379&q=80"
         imageAlt="a view of the University of Glasgow on a bright and cloudy day surrounded by trees"
         location="https://www.google.com/maps/place/Glasgow,+UK/@55.864237,-4.251806,12z/data=!4m13!1m7!3m6!1s0x488815562056ceeb:0x71e683b805ef511e!2sGlasgow,+UK!3b1!8m2!3d55.864237!4d-4.251806!3m4!1s0x488815562056ceeb:0x71e683b805ef511e!8m2!3d55.864237!4d-4.251806"
         description="I lived here for almost 18 years."
